ca5f56259786621133dc0095a6cf987afb0d4db0
matthew <peregoywork@gmail.com>
2026-08-15 16:49:45 -0700

    use api client

--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+  "fmt"
+  "time"
+ 
+  "ollama-gui/internal/api"
+)
 
 func main() {
-	fmt.Println("Hello, World")
+	fmt.Println("Server Startup")
+
+    rawURL := "http://localhost:11434"
+    rawToken := "secret-token"
+
+    client, err := api.NewClient(&rawURL, &rawToken, 5 * time.Second)
+    if err != nil {
+        fmt.Println("error starting ollama client", err)
+    }
+    
+    client.DoNothing()
+
+    fmt.Println("Server Exit")
 }
