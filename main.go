523ceda4100837fbfc7751d6612cf79e5347255c
matthew <peregoywork@gmail.com>
2026-08-15 21:11:38 -0700

    simplify to just running index file for now

--- main.go
+++ main.go
@@ -2,23 +2,28 @@ package main
 
 import (
   "fmt"
-  "time"
- 
-  "ollama-gui/internal/api"
+  // "time"
+  "net/http"
+  // "ollama-gui/internal/api"
 )
 
 func main() {
 	fmt.Println("Server Startup")
 
-    rawURL := "http://localhost:11434"
-    rawToken := "secret-token"
+    // rawURL := "http://localhost:11434"
+    // rawToken := "secret-token"
 
-    client, err := api.NewClient(&rawURL, &rawToken, 5 * time.Second)
-    if err != nil {
-        fmt.Println("error starting ollama client", err)
-    }
+    // client, err := api.NewClient(&rawURL, &rawToken, 5 * time.Second)
+    // if err != nil {
+    //     fmt.Println("error starting ollama client", err)
+    // }
     
-    client.DoNothing()
+    fileServer := http.FileServer(http.Dir("./static"))
+    http.Handle("/", fileServer)
+    err := http.ListenAndServe(":8080", nil)
+    if err != nil {
+        fmt.Println("error starting server", err)
+    }
 
     fmt.Println("Server Exit")
 }
